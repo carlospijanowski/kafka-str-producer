2111fd8c87af6be99fa909ec214d8ec93eebeeb9
Carlos Cartaxo <carlos.cartaxo@fcamara.com.br>
2023-01-22 15:40:56 -0300

    add call back

--- src/main/java/br/com/seteideias/strproducer/service/StringProducerService.java
+++ src/main/java/br/com/seteideias/strproducer/service/StringProducerService.java
@@ -5,6 +5,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+
+import static java.lang.System.out;
+
 @RequiredArgsConstructor
 @Service
 public class StringProducerService {
@@ -14,6 +18,12 @@ public class StringProducerService {
     private final KafkaTemplate<String, String> kafkaTemplate;
 
     public void sendMessage(String message) {
-        kafkaTemplate.send(nomeDoTopico,message);
+        kafkaTemplate.send(nomeDoTopico, message).addCallback(
+                sucesso -> {
+                    long timestamp = sucesso.getRecordMetadata().timestamp();
+                    out.println("sucesso ::: "+ timestamp);
+                },
+                falha -> out.println("falha ::: "+falha.getLocalizedMessage())
+        );
     }
 }
